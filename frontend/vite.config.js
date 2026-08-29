47fed950afdb6a3e6808099ff462bbef854eae9b
MUHAMMAD ASHRAF B. KAMARULZAMAN KPM-Guru <ashrafkrai@gmail.com>
2026-08-29 11:35:14 +0800

    Add CI/CD deployment config for Cloud Run + Firebase

---
+++ frontend/vite.config.js
@@ -0,0 +1,17 @@
+﻿import { defineConfig } from 'vite'
+
+export default defineConfig({
+  root: '.',
+  publicDir: false,
+  build: {
+    outDir: '../frontend-dist',
+    emptyOutDir: true
+  },
+  server: {
+    port: 5500,
+    proxy: {
+      '/api': 'http://127.0.0.1:8000',
+      '/video-feed': 'http://127.0.0.1:8000'
+    }
+  }
+})
